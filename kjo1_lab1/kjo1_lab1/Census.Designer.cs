dd1f230285c978a786257cb3199822f74fab2069
Kanghyun Jo <kjo1@ncstudents.niagaracollege.ca>
2019-09-26 00:54:52 -0400

    Done!

---
+++ kjo1_lab1/kjo1_lab1/Census.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace kjo1_lab1
+{
+    partial class Census
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Census));
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblRegion = new System.Windows.Forms.Label();
+            this.lblPopulation = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnClearData = new System.Windows.Forms.Button();
+            this.txtPopulation = new System.Windows.Forms.TextBox();
+            this.lblProRange = new System.Windows.Forms.Label();
+            this.lblRegRange = new System.Windows.Forms.Label();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbTitle.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbTitle.Location = new System.Drawing.Point(31, 26);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(199, 25);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Census Information";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Location = new System.Drawing.Point(36, 85);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(63, 13);
+            this.lblProvince.TabIndex = 1;
+            this.lblProvince.Text = "Province ID";
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Location = new System.Drawing.Point(36, 132);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(55, 13);
+            this.lblRegion.TabIndex = 2;
+            this.lblRegion.Text = "Region ID";
+            // 
+            // lblPopulation
+            // 
+            this.lblPopulation.AutoSize = true;
+            this.lblPopulation.Location = new System.Drawing.Point(39, 184);
+            this.lblPopulation.Name = "lblPopulation";
+            this.lblPopulation.Size = new System.Drawing.Size(57, 13);
+            this.lblPopulation.TabIndex = 3;
+            this.lblPopulation.Text = "Population";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(39, 226);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(155, 226);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(24, 274);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(90, 30);
+            this.btnSaveFile.TabIndex = 6;
+            this.btnSaveFile.Text = "Save to File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.BtnSaveFile_Click);
+            // 
+            // btnClearData
+            // 
+            this.btnClearData.Location = new System.Drawing.Point(155, 274);
+            this.btnClearData.Name = "btnClearData";
+            this.btnClearData.Size = new System.Drawing.Size(90, 30);
+            this.btnClearData.TabIndex = 7;
+            this.btnClearData.Text = "Clear Data";
+            this.btnClearData.UseVisualStyleBackColor = true;
+            this.btnClearData.Click += new System.EventHandler(this.BtnClearData_Click);
+            // 
+            // txtPopulation
+            // 
+            this.txtPopulation.Location = new System.Drawing.Point(106, 181);
+            this.txtPopulation.Name = "txtPopulation";
+            this.txtPopulation.Size = new System.Drawing.Size(124, 20);
+            this.txtPopulation.TabIndex = 3;
+            this.txtPopulation.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtPopulation_KeyPress);
+            // 
+            // lblProRange
+            // 
+            this.lblProRange.AutoSize = true;
+            this.lblProRange.Location = new System.Drawing.Point(212, 88);
+            this.lblProRange.Name = "lblProRange";
+            this.lblProRange.Size = new System.Drawing.Size(43, 13);
+            this.lblProRange.TabIndex = 11;
+            this.lblProRange.Text = "( 0 - 12)";
+            // 
+            // lblRegRange
+            // 
+            this.lblRegRange.AutoSize = true;
+            this.lblRegRange.Location = new System.Drawing.Point(215, 132);
+            this.lblRegRange.Name = "lblRegRange";
+            this.lblRegRange.Size = new System.Drawing.Size(43, 13);
+            this.lblRegRange.TabIndex = 12;
+            this.lblRegRange.Text = "( 0 -47 )";
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(106, 82);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 1;
+            this.txtProvince.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtProvince_KeyPress);
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Location = new System.Drawing.Point(106, 129);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(100, 20);
+            this.txtRegion.TabIndex = 2;
+            this.txtRegion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtRegion_KeyPress);
+            // 
+            // Census
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 324);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.lblRegRange);
+            this.Controls.Add(this.lblProRange);
+            this.Controls.Add(this.txtPopulation);
+            this.Controls.Add(this.btnClearData);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblPopulation);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lbTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Census";
+            this.RightToLeftLayout = true;
+            this.Text = "Census";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Label lblRegion;
+        private System.Windows.Forms.Label lblPopulation;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnClearData;
+        private System.Windows.Forms.TextBox txtPopulation;
+        private System.Windows.Forms.Label lblProRange;
+        private System.Windows.Forms.Label lblRegRange;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtRegion;
+    }
+}
+
---
+++ kjo1_lab1/kjo1_lab1/Census.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kjo1_lab1
+{
+    public partial class Census : Form
+    {
+        int[,] arrayPop = new int[13, 48]; // 13 rows, 48 cols
+
+        public Census()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            ClearAllText(this);
+            txtProvince.Focus();
+        }
+
+        private void BtnSaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            //sfd.FileName = "Canadian Census Information";
+            sfd.Filter = "Text Files|*.txt|AllFiles|*.*";
+            sfd.DefaultExt = "txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(sfd.FileName, FileMode.Create);
+
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(string.Format("Canadian Population Information " + DateTime.Now.ToString("(yyyy-MM-dd)\n")));
+                    for (int i = 0; i < 13; i++)
+                    {
+                        sw.WriteLine(string.Format("Province ID #{0}", i));
+                        for (int j = 0; j < 48; j++)
+                            sw.WriteLine(string.Format("\tRegion ID #{0}: {1}", new object[] { j, arrayPop[i, j] }));                        
+                    }
+
+                }
+            }
+            ClearAllData();
+        }
+
+        void ClearAllText(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                if (c is TextBox)
+                    ((TextBox)c).Clear();
+                else
+                    ClearAllText(c);
+            }
+        }
+
+        private void BtnClearData_Click(object sender, EventArgs e)
+        {
+            this.ClearAllData();
+            txtProvince.Focus();
+        }
+
+        void ClearAllData()
+        {
+            Array.Clear(arrayPop, 0, arrayPop.Length);
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            int provinceID;
+            int regionID;
+            if (!int.TryParse(txtProvince.Text, out provinceID) || provinceID > 12)
+            {
+                MessageBox.Show("Please enter a Province ID from between 0 to 12", "Error!");
+                txtProvince.Focus();
+            }
+
+            else if (!int.TryParse(txtRegion.Text, out regionID) || regionID > 47)
+            {
+                MessageBox.Show("Please enter a Regin ID between 0 to 47", "Error!");
+                txtRegion.Focus();
+            }
+            else if (!int.TryParse(txtPopulation.Text, out arrayPop[provinceID, regionID]))
+            {
+                MessageBox.Show("That's way too big!!!\n" +
+                    " Maximum accept input (2,147,483,647)\n" +
+                    "Canada 2019 population is estimated at 37,411,047 people at mid year according to UN data.\n" +
+                    " Canada population is equivalent to 0.49% of the total world population.", "For Your Information!");
+                txtPopulation.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Population: " + string.Format("{0:#,0}", Convert.ToInt32(txtPopulation.Text)) +
+                                "\nsaved for Prov #" + txtProvince.Text +
+                                ", Region #" + txtRegion.Text, "Success!");
+                ClearAllText(this);
+                txtProvince.Focus();
+            }
+        }
+
+
+
+        // not allow any key but numeric.
+        private void TxtRegion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxtPopulation_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxtProvince_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
